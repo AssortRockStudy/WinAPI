c5d1e31100b479e94a8e7f40bfd341de9155b0bf
lee <lee.jinyoung.k181122@gmail.com>
2023-09-05 11:18:11 +0900

    오류해결완료

--- jinyoung_client/main.cpp
+++ jinyoung_client/main.cpp
@@ -9,11 +9,11 @@
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
+HINSTANCE hInst=0;                                // 현재 인스턴스입니다.
 //MAX_LOADSTRING= 100;
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-HWND        g_hWnd = 0;
+//WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+//WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND g_hWnd = 0;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -42,7 +42,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     {
         return FALSE;
     }
-    CEngine::GetInst()->init(g_hWnd, POINT{ 1280,200 });
+    CEngine::GetInst()->init(g_hWnd, POINT{ 1280,800 });
 
 
     //단축키 테이블 참조
@@ -126,7 +126,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
     //메뉴바
     wcex.lpszMenuName =   nullptr; // MAKEINTRESOURCEW(IDC_JINYOUNGCLIENT);
-    wcex.lpszClassName  = szWindowClass;
+    wcex.lpszClassName = L"MyWindow";
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
