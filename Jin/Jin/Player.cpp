f527407563b27288e66d9622d3e884a8b4427d3b
woo <woo060874@naver.com>
2023-09-06 16:10:41 +0900

    #4 플레이어 객체 생성 검정색 사각형으로 색 변경

--- Jin/Jin/Player.cpp
+++ Jin/Jin/Player.cpp
@@ -45,24 +45,23 @@ void Player::render(HDC _dc)
 	Vec2 vPos = GetPos();
 	Vec2 vScale = GetScale();
 
-	// Black Pen -> Red Pen
-	HPEN hRedPen = CreatePen(PS_SOLID, 1, RGB(255, 20, 20));
-	HPEN hPrevPen = (HPEN)SelectObject(_dc, hRedPen);
+	HPEN hBlackPen = CreatePen(PS_SOLID, 1, RGB(0,0,0));
+	HPEN hPrevPen = (HPEN)SelectObject(_dc, hBlackPen);
 
-	// White Brush -> Blue Brush
-	HBRUSH hBlueBrush = CreateSolidBrush(RGB(20, 20, 255));
-	HBRUSH hPrevBrush = (HBRUSH)SelectObject(_dc, hBlueBrush);
-
+	HBRUSH hBlackBrush = CreateSolidBrush(RGB(0, 0, 0));
+	HBRUSH hPrevBrush = (HBRUSH)SelectObject(_dc, hBlackBrush);
 	Rectangle(_dc
 		, int(vPos.x - vScale.x / 2)
 		, int(vPos.y - vScale.y / 2)
 		, int(vPos.x + vScale.x / 2)
 		, int(vPos.y + vScale.y / 2));
 
-	// 되돌리고 사용했던 펜, 브러쉬를 삭제한다.
-	SelectObject(_dc, hPrevPen);
-	DeleteObject(hRedPen);
 
-	SelectObject(_dc, hPrevBrush);
-	DeleteObject(hBlueBrush);
+
+	//// 되돌리고 사용했던 펜, 브러쉬를 삭제한다.
+	//SelectObject(_dc, hPrevPen);
+	//DeleteObject(hRedPen);
+
+	//SelectObject(_dc, hPrevBrush);
+	//DeleteObject(hBlueBrush);
 }
