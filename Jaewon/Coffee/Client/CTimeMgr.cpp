11e35c73639f840eceb53327c2d83b24dd35b954
kimjw395 <01kimjw@kookmin.ac.kr>
2023-09-08 09:32:39 +0900

    델타 타임 계산을 위한 타임 매니져 생성

---
+++ Jaewon/Coffee/Client/CTimeMgr.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CTimeMgr.h"
+
+CTimeMgr::CTimeMgr() :mFrequency(), mPrevCnt(), mCurCnt(){}
+CTimeMgr::~CTimeMgr(){}
+
+void CTimeMgr::init()
+{
+	QueryPerformanceFrequency(&mFrequency);
+	QueryPerformanceCounter(&mPrevCnt);
+}
+
+void CTimeMgr::tick()
+{
+	// tick마다 delta time 계산
+	QueryPerformanceCounter(&mCurCnt);
+	mDeltaT = float(mCurCnt.QuadPart - mPrevCnt.QuadPart) / float(mFrequency.QuadPart);
+	mPrevCnt = mCurCnt;
+}
