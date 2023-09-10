cc5d0c3cf4cd2ebc1f867f03b96a5e6c542198c5
lee <lee.jinyoung.k181122@gmail.com>
2023-09-10 14:55:27 +0900

    #4 "메소익스플로전" 구현

--- jinyoung_client/CProjectile.cpp
+++ jinyoung_client/CProjectile.cpp
@@ -8,6 +8,7 @@
 CProjectile::CProjectile()
 	: m_Speed(0.f)
 	, m_theta(PI / 2.f)
+	, m_mass(5.f)
 {
 }
 
@@ -15,18 +16,41 @@ CProjectile::~CProjectile()
 {
 }
 
+
 void CProjectile::tick(float _DT)
 {
 	Vec2 vPos = GetPos();
-	CMonster* cm= CLevelMgr::GetInst()->GetCurLevel()->getMonster();
 	
-	m_theta = atan((cm->GetPos().y - vPos.y) / (cm->GetPos().x - vPos.x));
 	
+	addforce(10000.f, _DT);
 
 	vPos.x += m_Speed * cosf(m_theta) * _DT;
-	vPos.y += m_Speed * sinf(m_theta) * _DT;
+	//y의 증가량이 -이기때문
+	vPos.y -= m_Speed * sinf(m_theta) * _DT;
 
 	SetPos(vPos);
+}
+
+
+void CProjectile::addforce(float _force , float _DT)
+{
+
+	float a = _force / m_mass;
+
+	Vec2 vPos = GetPos();
+
+	CMonster* cm = CLevelMgr::GetInst()->GetCurLevel()->getMonster();
+
+	float targetth = atan2((-1) * (cm->GetPos().y - vPos.y), (cm->GetPos().x - vPos.x));
+
+	int x = m_Speed * cosf(m_theta) + (cosf(targetth)* a *_DT);
+
+	//y의 증가량이 -이기때문
+	int y = m_Speed * sinf(m_theta) + (sinf(targetth)* a * _DT);
+
+	m_Speed = sqrtf(pow(x,2) + pow(y,2));
+	m_theta = atan2(y, x);
+
 }
 
 void CProjectile::render(HDC _dc)
--- jinyoung_client/CProjectile.h
+++ jinyoung_client/CProjectile.h
@@ -8,12 +8,14 @@ class CProjectile :
 private:
     float   m_Speed;
     float   m_theta;
+    float   m_mass;
 
 public:
     void SetDir(float _theta) { m_theta = _theta; }
     void SetSpeed(float _Speed) { m_Speed = _Speed; }
     __inline void Settheta(float _theta) { m_theta = _theta; }
     __inline float Gettheta() { return m_theta; }
+    void addforce(float _force , float _DT);
 
 public:
     virtual void tick(float _DT) override;
