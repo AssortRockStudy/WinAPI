a9ad14ea08facccb7a7360a53410a28afc7179d0
jeontigger <boy9609@naver.com>
2023-10-15 15:06:25 +0900

    #24 금요일 2교시 FollowUp 보충

--- inho/inho/CPanelUI.cpp
+++ inho/inho/CPanelUI.cpp
@@ -1,2 +1,18 @@
 ﻿#include "pch.h"
 #include "CPanelUI.h"
+
+void CPanelUI::tick(float _DT)
+{
+}
+
+void CPanelUI::render(HDC _dc)
+{
+}
+
+CPanelUI::CPanelUI()
+{
+}
+
+CPanelUI::~CPanelUI()
+{
+}
--- inho/inho/CPanelUI.h
+++ inho/inho/CPanelUI.h
@@ -3,5 +3,16 @@
 class CPanelUI :
     public CUI
 {
+    GENERATED_OBJECT(CUI);
+
+private:
+
+public:
+    virtual void tick(float _DT) override;
+    virtual void render(HDC _dc) override;
+
+public:
+    CPanelUI();
+    ~CPanelUI();
 };
 
